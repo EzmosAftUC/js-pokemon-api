4263faaaa782c4a9b43f9218b9510206799be667
Moses <mosesfat@gmail.com>
2022-02-11 20:24:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+//
+import Axios from 'axios';
 
 function App() {
+
+  const [pokemon, setPoke] = useState("");
+  const [poke, setPokemon] = useState({
+    name: "", 
+    species: "", 
+    img: "", 
+    hp: "",
+    weight: "",
+    height: "",
+    base: "",
+  });
+
+  const searchPoke = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) =>
+    {
+      setPokemon({
+        name: pokemon, 
+        species: response.data.species.name, 
+        img: response.data.sprites.front_default, 
+        weight: response.data.weight,
+        height: response.data.height,
+        base: response.data.base_experience,
+        hp: response.data.stats[0].base_stat});
+    });
+  }
+
   return (
+  <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="header">
+        <h1 className="title">Pokedex</h1>
+          <input type="text" onChange={(event) => {setPoke(event.target.value);}}/>
+          <button onClick={searchPoke}>submit</button>
+      </div>
+          <div className="display">
+
+            <div className="info">
+              <h1>Name: {poke.name}</h1>
+              <h1>Species: {poke.species}</h1>
+              <h1>HP: {poke.hp}</h1>
+              <h1>Weight: {poke.weight}</h1>
+              <h1>Height: {poke.height}</h1>
+              <h1>Base Experience: {poke.base}</h1>
+            </div>
+
+            <div className="pokePic">
+              <img src={poke.img} alt="pokepic"/>
+            </div>
+
+          </div>
+    </div>    
+  </>
   );
 }
 
